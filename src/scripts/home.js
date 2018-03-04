9dbf83d438b900010bb103c8ea47f126ba89ee6a
JanistaSrilerjan <janista_sr@cmu.ac.th>
2018-03-04 20:22:58 +0700

    call q [front-end]

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -29,3 +29,36 @@ function next() {
 
     document.getElementById("num_q").innerHTML = next_q.toString();
 }
+var i=0;
+function call_q(){
+    var inpObj = document.getElementById("cust_num");
+    var q = document.getElementById("cust_num").value;
+    if (!inpObj.checkValidity()) {
+        document.getElementById("empty_q").innerHTML = "กรุณาระบุหมายเลขคิว";
+    } else if(q < 1) {
+        document.getElementById("empty_q").innerHTML = "ระบุหมายเลขคิวไม่ถูกต้อง";
+    }
+    else{
+        document.getElementById("empty_num").style.display = 'none';
+        
+        var original = document.getElementById('show_call');      
+        var j=0;
+        if(i==0){
+            document.getElementById("call_num").innerHTML = q.toString();
+            document.getElementById("show_call").style.display = 'block';
+            i++;
+        }
+        else{
+            document.getElementById("call_num").innerHTML = q.toString();
+            var clone = original.cloneNode(true); // "deep" clone
+            clone.id = "duplicater" + ++j;
+            original.parentNode.appendChild(clone);
+        }
+        console.log(i);
+    }
+}
+
+function cancel(){
+    document.getElementById("show_call").style.display = 'none';
+    i = 0;
+}
